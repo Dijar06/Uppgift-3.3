8752252cf2658de536763cdc277e5de123267471
Dijar06 <dize06001@utb.vaxjo.se>
2023-10-06 12:46:00 +0200

    Add project files.

---
+++ Uppgift 3.3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Uppgift3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många timmar vill du hyra bilen?");
+            int timmar = int.Parse(Console.ReadLine());
+            int pris = timmar * 80;
+
+            if (pris > 950)
+            {
+                Console.WriteLine("Priset blir 950 kronor.");
+            }
+            else
+            {
+                Console.WriteLine("Priset blir " + pris);
+            }
+        }
+    }
+}
